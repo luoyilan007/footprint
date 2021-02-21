9904bba2b00eed7cedf6368c0c83f41f3771671c
arvinxx <arvinx@foxmail.com>
2021-02-21 13:23:46 +0800

    :white_check_mark: test: 补充书单测试

--- tests/zotero.spec.ts
+++ tests/zotero.spec.ts
@@ -2,7 +2,17 @@ import BooksList from '../api/books/[year]';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 describe('Zotero 书单', () => {
-  test('2021年书单', async () => {
+  it('2020年书单', async () => {
+    const { raw } = await BooksList(
+      <VercelRequest>(<unknown>{ query: { year: 2020 } }),
+      <VercelResponse>(<unknown>{
+        json: () => {},
+      }),
+    );
+    expect(raw.key).toEqual('22VU3NNR');
+    expect(raw.name).toEqual('2020年书单');
+  });
+  it('2021年书单', async () => {
     const { raw } = await BooksList(
       <VercelRequest>(<unknown>{ query: { year: 2021 } }),
       <VercelResponse>(<unknown>{
