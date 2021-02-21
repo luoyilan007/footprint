d97530c88da649d07ea7a17b55d677c7e726e647
arvinxx <arvinx@foxmail.com>
2021-02-21 23:25:13 +0800

    :lipstick: style: 调整主题色和加载样式

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -7,13 +7,13 @@ type DefaultSettings = Partial<ProSettings> & {
 const proSettings: DefaultSettings = {
   navTheme: 'light',
   // 拂晓蓝
-  primaryColor: '#1890ff',
+  primaryColor: '#40c0e5',
   layout: 'side',
   contentWidth: 'Fluid',
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Arvin Studio',
+  title: '空谷的足迹',
   pwa: false,
   iconfontUrl: '',
 };
--- src/components/PageLoading/index.tsx
+++ src/components/PageLoading/index.tsx
@@ -1,5 +1,44 @@
-import { PageLoading } from '@ant-design/pro-layout';
+import React, { useEffect } from 'react';
+import classNames from 'classnames';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
-// loading components from code split
-// https://umijs.org/plugin/umi-plugin-react.html#dynamicimport
+import styles from './style.less';
+
+const PageLoading: React.FC = () => {
+  useEffect(() => {
+    NProgress.configure({
+      showSpinner: false,
+    });
+
+    NProgress.start();
+    NProgress.inc();
+
+    return () => {
+      NProgress.done();
+    };
+  }, []);
+  return (
+    <div className={styles.body}>
+      <div className={styles.ctnr}>
+        <div className={styles.ldr}>
+          <div className={styles.ldrBlk} />
+          <div
+            className={classNames({
+              [styles.ldrBlk]: true,
+              [styles.anDelay]: true,
+            })}
+          />
+          <div
+            className={classNames({
+              [styles.ldrBlk]: true,
+              [styles.anDelay]: true,
+            })}
+          />
+          <div className={styles.ldrBlk} />
+        </div>
+      </div>
+    </div>
+  );
+};
 export default PageLoading;
