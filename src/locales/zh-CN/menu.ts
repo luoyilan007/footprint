4cf8ec03c01f9c1213599a0ad15e3091e7680477
arvinxx <arvinx@foxmail.com>
2021-03-07 02:23:21 +0800

    :fire: chore: 清理无用代码

--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -2,6 +2,8 @@ export default {
   'menu.welcome': '欢迎',
   'menu.more-blocks': '更多区块',
   'menu.home': '首页',
+  'menu.playground': '测试场',
+  'menu.playground.pdf': 'PDF 评论',
   'menu.admin': '管理页',
   'menu.admin.sub-page': '二级管理页',
   'menu.login': '登录',
